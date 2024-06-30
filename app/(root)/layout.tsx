121ca12b7066361bb75aba2bf9c278bcb6d46109
Rajaaabpro <rajaab.pro@gmail.com>
2024-06-30 16:44:56 +0500

    Fix the issue

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,8 +4,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <main>
-      SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
       {children}
     </main>
   );
